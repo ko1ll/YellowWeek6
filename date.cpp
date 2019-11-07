547d772d26b9cb9efbf578b733d22a1d75f6255c
Daniil Kirpichenko <ko1lmain4@gmail.com>
2019-11-07 19:00:18 +0300

    class Date for date

--- date.cpp
+++ date.cpp
@@ -1,4 +1,20 @@
 #include "date.h"
 
 
-
+ostream& operator <<(ostream& stream, const Date& date) {
+	stream << setfill('0');
+	stream << setw(4) << date.year_ << "-"
+		<< setw(2) << date.month_ << "-"
+		<< setw(2) << date.day_ << " ";
+	return stream;
+}
+Date ParseDate(istream& stream) {
+	int day, year, month;
+	stream >> year;
+	stream.ignore(1);
+	stream >> month;
+	stream.ignore(1);
+	stream >> day;
+	stream.ignore(1);
+	return Date(year, month, day);
+};
--- date.h
+++ date.h
@@ -1,12 +1,21 @@
 ﻿#pragma once
 #include <string>
+#include <sstream>
+#include <iomanip>
 using namespace std;
 //date.h/cpp — эти файлы должны содержать объявления и определения класса Date,
 //функции ParseDate и оператора вывода в поток для класса Date;
 class Date {
 public:
-	
+	Date(const int& year,
+		const int& month, const int& day) :
+		day_(day), year_(year), month_(month) {};
+	const int day_;
+	const int year_;
+	const int month_;
 };
 
 
-string ParseDate(istream& stream);
+Date ParseDate(istream& stream);
+
+ostream& operator <<(ostream& stream, const Date& date);
